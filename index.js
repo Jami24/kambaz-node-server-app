463c65598229209929415303294288f0ed2a0809
Jami24 <choong.e@northeastern.edu>
2026-05-26 09:55:21 -0400

    CORS fix

--- index.js
+++ index.js
@@ -32,11 +32,12 @@ const sessionOptions = {
 };
 
 if (process.env.SERVER_ENV !== "development") {
+    app.set("trust proxy", 1);
+
     sessionOptions.proxy = true;
     sessionOptions.cookie = {
         sameSite: "none",
-        secure: true,
-        domain: process.env.SERVER_URL,
+        secure: true
     };
 }
 
